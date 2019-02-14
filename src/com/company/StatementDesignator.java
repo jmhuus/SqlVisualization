e31b6bff3144ddb561362af246b817765bbf4ab6
Jordan H <jordanhuusy@live.com>
2019-02-13 23:03:31 -0800

    adding SQL parser Java code

---
+++ src/com/company/StatementDesignator.java
@@ -0,0 +1,158 @@
+package com.company;
+
+import net.sf.jsqlparser.statement.*;
+import net.sf.jsqlparser.statement.alter.Alter;
+import net.sf.jsqlparser.statement.comment.Comment;
+import net.sf.jsqlparser.statement.create.index.CreateIndex;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+import net.sf.jsqlparser.statement.create.view.AlterView;
+import net.sf.jsqlparser.statement.create.view.CreateView;
+import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.statement.drop.Drop;
+import net.sf.jsqlparser.statement.execute.Execute;
+import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.merge.Merge;
+import net.sf.jsqlparser.statement.replace.Replace;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectBody;
+import net.sf.jsqlparser.statement.truncate.Truncate;
+import net.sf.jsqlparser.statement.update.Update;
+import net.sf.jsqlparser.statement.upsert.Upsert;
+import net.sf.jsqlparser.statement.values.ValuesStatement;
+import net.sf.jsqlparser.util.TablesNamesFinder;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StatementDesignator implements StatementVisitor {
+    private ArrayList<DiagramNode> diagramNodes;
+
+    public StatementDesignator(ArrayList<DiagramNode> diagramNodes) {
+        this.diagramNodes = diagramNodes;
+    }
+
+    @Override
+    public void visit(Comment comment) {
+
+    }
+
+    @Override
+    public void visit(Commit commit) {
+
+    }
+
+    @Override
+    public void visit(Delete delete) {
+
+    }
+
+    @Override
+    public void visit(Update update) {
+
+    }
+
+    @Override
+    public void visit(Insert insert) {
+
+    }
+
+    @Override
+    public void visit(Replace replace) {
+
+    }
+
+    @Override
+    public void visit(Drop drop) {
+
+    }
+
+    @Override
+    public void visit(Truncate truncate) {
+
+    }
+
+    @Override
+    public void visit(CreateIndex createIndex) {
+
+    }
+
+    @Override
+    public void visit(CreateTable createTable) {
+
+    }
+
+    @Override
+    public void visit(CreateView createView) {
+
+    }
+
+    @Override
+    public void visit(AlterView alterView) {
+
+    }
+
+    @Override
+    public void visit(Alter alter) {
+
+    }
+
+    @Override
+    public void visit(Statements statements) {
+
+    }
+
+    @Override
+    public void visit(Execute execute) {
+
+    }
+
+    @Override
+    public void visit(SetStatement setStatement) {
+
+    }
+
+    @Override
+    public void visit(ShowStatement showStatement) {
+
+    }
+
+    @Override
+    public void visit(Merge merge) {
+
+    }
+
+    @Override
+    public void visit(Select select) {
+        TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
+        List tableList = tablesNamesFinder.getTableList(select);
+        for (Iterator iter = tableList.iterator(); iter.hasNext(); ) {
+            System.out.println(iter.next());
+        }
+
+
+        // Retrieve FROM tables, INTO tables, and SELECT items
+        SelectBody selectBody = select.getSelectBody();
+        selectBody.accept(new SelectDesignator());
+    }
+
+    @Override
+    public void visit(Upsert upsert) {
+
+    }
+
+    @Override
+    public void visit(UseStatement useStatement) {
+
+    }
+
+    @Override
+    public void visit(Block block) {
+
+    }
+
+    @Override
+    public void visit(ValuesStatement valuesStatement) {
+
+    }
+}
