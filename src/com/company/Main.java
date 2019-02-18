d614fe8313bd53bf8d271f48f6718ca6e9040fd2
Jordan H <jordanhuusy@live.com>
2019-02-18 15:06:29 -0800

    beginning to add mechanics to inject multiple queries

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
 import net.sf.jsqlparser.statement.Statement;
 
-import java.io.StringReader;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
 
@@ -13,12 +19,44 @@ public class Main {
             // All nodes tracked within DiagramNodeManager
             DiagramNodeManager diagramNodeManager = new DiagramNodeManager();
             DiagramNode diagramNode = new DiagramNode();
+            List<String> sqlCommands = new ArrayList<>();
+            sqlCommands.add("ALTER");
+            sqlCommands.add("COMMENT");
+            sqlCommands.add("CREATE");
+            sqlCommands.add("DELETE");
+            sqlCommands.add("DROP");
+            sqlCommands.add("EXECUTE");
+            sqlCommands.add("INSERT");
+            sqlCommands.add("MERGE");
+            sqlCommands.add("REPLACE");
+            sqlCommands.add("SELECT");
+            sqlCommands.add("TRUNCATE");
+            sqlCommands.add("UPDATE");
+            sqlCommands.add("UPSERT");
+            sqlCommands.add("VALUES");
 
 
             /** TODO: use keyword SQL starting statements to recognize new queries
              * ALTER, COMMENT, CREATE, DELETE, DROP, EXECUTE, INSERT, MERGE
              * REPLACE, SELECT, TRUNCATE, UPDATE, UPSERT, VALUES
              */
+            File file = new File("./TestSqlScript.sql");
+            List<String> lines = FileUtils.readLines(file, "UTF-8");
+            for(String line: lines){
+                if (line.contains("SELECT")){
+
+                }else if(line.contains("DELETE")){
+
+                }else if(line.contains("ALTER")){
+
+                }else if(line.contains("CREATE")){
+
+                }else if(line.contains("UPDATE")){
+
+                }
+            }
+
+
 
             // Parse and return select statement items
             CCJSqlParserManager pm = new CCJSqlParserManager();
