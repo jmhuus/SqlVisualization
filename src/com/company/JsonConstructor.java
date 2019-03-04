21dee816b2372939a7ede30b55be477a5f1593d5
Fine-ants <jordanhuusy@live.com>
2019-03-04 13:02:01 -0800

    first (broken) version of constructing JSON diagram data

--- src/com/company/JsonConstructor.java
+++ src/com/company/JsonConstructor.java
@@ -1,5 +1,8 @@
 package com.company;
 
+
+import org.json.JSONObject;
+
 public class JsonConstructor {
     private DiagramNodeManager diagramNodeManager;
 
@@ -8,4 +11,53 @@ public class JsonConstructor {
     }
 
 
+    public String getJsonDiagram(){
+
+        JSONObject jsonData = new JSONObject();
+
+
+        // Nodes without parents begin recursive statement
+        for(String nodeName: diagramNodeManager.getDiagramNodes().keySet()){
+            if(diagramNodeManager.getDiagramNode(nodeName).getParentNodes().size()==0){
+                jsonData.put(nodeName, getChildJsonData(diagramNodeManager, nodeName));
+            }
+        }
+
+        return jsonData.toString();
+    }
+
+
+    /**
+     * Recursive method used to gather all child JSON diagram data.
+     * @param diagramNodeManager
+     * @param rootName
+     * @return a nested JSON object; the root JSON object contains child objects.
+     */
+    private JSONObject getChildJsonData(DiagramNodeManager diagramNodeManager, String rootName){
+
+        // Init new JSONObject for child nodes
+        JSONObject childJsonData = new JSONObject();
+
+        // Children
+        for(DiagramNode childNode: diagramNodeManager.getDiagramNode(rootName).getChildNodes()){
+            childJsonData.put(childNode.getNodeName(), getChildJsonData(diagramNodeManager, childNode.getNodeName()));
+        }
+
+        return childJsonData;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -95,13 +95,13 @@ public class Main {
 
 
         // TEST: JSON output
-        String jsonString = new JSONObject()
-                .put("JSON1", "Hello World!")
-                .put("JSON2", "Hello my World!")
-                .put("JSON3", new JSONObject()
-                        .put("key1", "value1")).toString();
-
-        System.out.println(jsonString);
-
+        JsonConstructor jsonConstructor = new JsonConstructor(diagramNodeManager);
+        System.out.println(jsonConstructor.getJsonDiagram());
+//        String jsonString = new JSONObject()
+//                .put("JSON1", "Hello World!")
+//                .put("JSON2", "Hello my World!")
+//                .put("JSON3", new JSONObject()
+//                        .put("key1", "value1")).toString();
+//        System.out.println(jsonString);
     }
 }
