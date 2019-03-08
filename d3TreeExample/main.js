6c5082e4103bcebc31baa6b8cc0d2fd346a8a9f7
Fine-ants <jordanhuusy@live.com>
2019-03-07 19:57:21 -0800

    changed css to fill html body height to screen size

--- d3TreeExample/main.js
+++ d3TreeExample/main.js
@@ -16,8 +16,8 @@ var diagonal = d3.svg.diagonal()
 	.projection(function(d) { return [d.x, d.y]; });
 
 var svg = d3.select("body").append("svg")
-	.attr("width", width + margin.right + margin.left)
-	.attr("height", height + margin.top + margin.bottom)
+	.attr("width", "100%")
+	.attr("height", "100%")
   .append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
